4edaf972be8df1db036596a4f9b4d6e91b581114
Jovin2301 <s10243666@connect.np.edu.sg>
2024-01-07 16:47:34 +0800

    commented back

--- models/raw/raw_order.sql
+++ models/raw/raw_order.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM {{source('NWT', 'ORDERS')}}
 
-UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
+/*UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
 WHERE orderdate < '1900-01-01';
 
 UPDATE raw_order SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
@@ -11,4 +11,4 @@ WHERE requireddate < '1900-01-01';
 UPDATE raw_order SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
 WHERE shippeddate < '1900-01-01';
 
-select * from raw_order
+select * from raw_order*/
