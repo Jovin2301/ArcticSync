7ab00e38b3752f2b3acf54aaa2de67f6cfc740d2
Sam <samuelkoh17@gmail.com>
2024-01-12 02:46:18 +0800

    Switched to dbt core, did dbt init, did snowflake connection, and profiles.yml

--- models/reporting/report_roi_by_category.sql
+++ models/reporting/report_roi_by_category.sql
@@ -1,2 +1,6 @@
 {{ config(  materialized='table') }}
-SELECT * FROM {{ref('raw_category')}}
+
+-- This is a SQL file, not a dbt model file
+SELECT CategoryName, AVG(UnitPrice) as AvgUnitPrice
+FROM {{ ref('samuel_staging') }}
+GROUP BY CategoryName
