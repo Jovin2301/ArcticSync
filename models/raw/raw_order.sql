8000619d1f35df1123027d061ffbaaa6b5c43daa
Kendrick Tan <s10244287@connect.np.edu.sg>
2024-01-12 09:39:23 +0000

    increment codes for fresh order and order details

--- models/raw/raw_order.sql
+++ models/raw/raw_order.sql
@@ -1,14 +1,24 @@
-{{ config (materialized='table')}}
+--{{ config (materialized='table')}}
 
-SELECT * FROM {{source('NWT', 'ORDERS')}}
+--SELECT * FROM {{source('NWT', 'ORDERS')}}
 
-/*UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
-WHERE orderdate < '1900-01-01';
+-- /*UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
+-- WHERE orderdate < '1900-01-01';
 
-UPDATE raw_order SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
-WHERE requireddate < '1900-01-01';
+-- UPDATE raw_order SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
+-- WHERE requireddate < '1900-01-01';
 
-UPDATE raw_order SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
-WHERE shippeddate < '1900-01-01';
+-- UPDATE raw_order SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
+-- WHERE shippeddate < '1900-01-01';
 
-select * from raw_order*/
+-- select * from raw_order*/
+
+{{ config(materialized='incremental', unique_key='ORDERID') }}
+
+SELECT *
+FROM {{ ref ('fresh_orders') }}
+{% if is_incremental() %}
+WHERE CAST(ORDERID AS BIGINT) > (SELECT MAX(CAST(ORDERID AS BIGINT))  FROM {{this}})
+{% endif %}
+
+--select * from raw_order
--- models/raw/raw_order_details.sql
+++ models/raw/raw_order_details.sql
@@ -1,3 +1,14 @@
-{{ config (materialized='table')}}
+-- {{ config (materialized='table')}}
 
-SELECT * FROM {{ source('NWT', 'ORDER_DETAIL')}}
+-- SELECT * FROM {{ source('NWT', 'ORDER_DETAIL')}}
+
+
+
+{{ config(materialized='incremental', unique_key='ORDERID') }}
+
+SELECT *
+FROM {{ ref('fresh_order_detail') }}
+{% if is_incremental() %}
+WHERE (CAST(ORDERID AS BIGINT)) > 
+      (SELECT MAX(CAST(ORDERID AS BIGINT)) FROM {{this}})
+{% endif %}
