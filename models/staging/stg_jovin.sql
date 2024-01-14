1210ca76f5127597d3f0055589edee3df74ccef0
Jovin2301 <s10243666@connect.np.edu.sg>
2024-01-14 13:48:57 +0800

    edited the names of the reporting files

--- models/staging/stg_jovin.sql
+++ models/staging/stg_jovin.sql
@@ -2,21 +2,27 @@ SELECT
 -- from raw_order_details
 d.orderID, 
 d.productID,
-d.unitPrice,
+d.unitPrice, -- the price of product that customer bought the item
 quantity,
 discount, 
+
 --from raw_customer
 customerID, 
 country,
+
 -- from raw_order
 o.employeeID, 
 orderDate, 
 requiredDate,
 shipCountry,
 shipCity,
+freight,
+shipVia,
+
 --from raw_product
 productName, 
-p.unitPrice as costPrice,
+p.unitPrice -- the current pricing of the product
+
 --from raw_employee
 lastName, 
 firstName, 
@@ -25,11 +31,18 @@ titleOfCourtesy,
 photo, 
 reportsTo,
 photoPath,
+
 --from raw_category
 categoryName, 
 picture, 
-(d.unitPrice * (1-discount) * quantity) as productPrice,
-(p.unitPrice * quantity) as productCost
+
+-- additional calculation
+((d.unitPrice * (1-discount) * quantity) + freight) as productPrice, -- total cost for each of the product at the time of purchase
+(p.unitPrice * quantity * (1-discount)) as totalSales, -- the price of per product 
+-- ((d.unitPrice - unitCost) * quantity * (1-discount)) as profitMargin, 
+
+
+
 FROM
 {{ ref ('raw_order')}} as o
 
