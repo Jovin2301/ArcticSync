41069fd46aed2444ce06fb95d6c1deb582f35502
Sam <samuelkoh17@gmail.com>
2024-01-20 14:39:36 +0800

    Changed ref to stg_samuel (renamed version)

--- models/reporting/report_sales_by_region_SAMUEL.sql
+++ models/reporting/report_sales_by_region_SAMUEL.sql
@@ -2,5 +2,5 @@
 
 -- This is a SQL file, not a dbt model file
 SELECT RegionDescription, AVG(UnitPrice) as AvgUnitPrice
-FROM {{ ref('samuel_staging') }}
+FROM {{ ref('stg_samuel') }}
 GROUP BY RegionDescription
