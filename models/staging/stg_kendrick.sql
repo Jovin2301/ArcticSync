cd67877d5b0bcc3679ba0fbe36bff0fd3ddf6308
kendrick61934 <mingrui15@gmail.com>
2024-01-21 00:43:46 +0800

    name changes to stg_kendrick by Kendrick

--- models/staging/stg_kendrick.sql
+++ models/staging/stg_kendrick.sql
@@ -35,6 +35,9 @@ SELECT
     WHEN DATEDIFF('day', o.shippeddate, o.requireddate) >= 0 THEN 1
     ELSE 0
   END AS on_time_delivery,
+    
+  SUM(on_time_delivery) AS on_time_deliveries,
+  SUM(on_time_delivery) / COUNT(DISTINCT orderid) AS on_time_delivery_rate,
 
   -- from raw_category (product detail)
   c.categoryID,
