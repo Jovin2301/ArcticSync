8d6faf4d0bd5906d645ecb01c0228ce9619cb2f4
Sam <samuelkoh17@gmail.com>
2024-01-20 15:26:31 +0800

    trying to use a dedicated SQL file to check for dates that are less than the year 1900

---
+++ models/raw/raw_order_update.sql
@@ -0,0 +1,32 @@
+/*Trying to fix the raw_test.yml test case where we replace order date that is
+earlier than 1900, to 1900 */
+
+merge into raw_order
+using (
+  select ORDERID, DATEADD(YEAR, 1900, orderdate) as orderdate2
+  from raw_order
+  where orderdate < '1900-01-01'
+) as src
+on raw_order.ORDERID = src.ORDERID
+when matched then
+  update set raw_order.orderdate = src.orderdate2;
+
+merge into raw_order
+using (
+  select ORDERID, DATEADD(YEAR, 1900, requireddate) as requireddate2
+  from raw_order
+  where requireddate < '1900-01-01'
+) as src
+on raw_order.ORDERID = src.ORDERID
+when matched then
+  update set raw_order.requireddate = src.requireddate2;
+
+merge into raw_order
+using (
+  select ORDERID, DATEADD(YEAR, 1900, shippeddate) as shippeddate2
+  from raw_order
+  where shippeddate < '1900-01-01'
+) as src
+on raw_order.ORDERID = src.ORDERID
+when matched then
+  update set raw_order.shippeddate = src.shippeddate2;
