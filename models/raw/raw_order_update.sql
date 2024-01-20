0ff2928271606799326544aa0abed4a4038ed376
Sam <samuelkoh17@gmail.com>
2024-01-20 23:43:52 +0800

    Corrected issue where in raw_order_update, column aliases in the subqueries are not consistent with update statements

--- models/raw/raw_order_update.sql
+++ models/raw/raw_order_update.sql
@@ -1,39 +1,32 @@
-/*Trying to fix the raw_test.yml test case where we replace order date that is
-earlier than 1900, to 1900 */
-
 -- MERGING UPDATES FOR ORDER DATE
-merge into raw_order
-using (
-  select ORDERID, DATEADD(YEAR, 1900, orderdate) as orderdate2
-  from raw_order
-  where orderdate < '1900-01-01'
-) as src
-on raw_order.ORDERID = src.ORDERID
-when matched then
-  update set raw_order.orderdate = src.orderdate2;
+MERGE INTO raw_order
+USING (
+    SELECT ORDERID, DATEADD(YEAR, 1900, orderdate) AS orderdate2
+    FROM raw_order
+    WHERE orderdate < '1900-01-01'
+) AS src
+ON raw_order.ORDERID = src.ORDERID
+WHEN MATCHED THEN
+    UPDATE SET raw_order.orderdate = src.orderdate2;
 
 -- MERGING UPDATES FOR REQUIRED DATE
-merge into raw_order
-using (
-  select ORDERID, DATEADD(YEAR, 1900, requireddate) as requireddate2
-  from raw_order
-  where requireddate < '1900-01-01'
-) as src
-on raw_order.ORDERID = src.ORDERID
-when matched then
-  update set raw_order.requireddate = src.requireddate2;
+MERGE INTO raw_order
+USING (
+    SELECT ORDERID, DATEADD(YEAR, 1900, requireddate) AS requireddate2
+    FROM raw_order
+    WHERE requireddate < '1900-01-01'
+) AS src
+ON raw_order.ORDERID = src.ORDERID
+WHEN MATCHED THEN
+    UPDATE SET raw_order.requireddate = src.requireddate2;
 
 -- MERGING UPDATES FOR SHIPPED DATE
-merge into raw_order
-using (
-  select ORDERID, DATEADD(YEAR, 1900, shippeddate) as shippeddate2
-  from raw_order
-  where shippeddate < '1900-01-01'
-) as src
-on raw_order.ORDERID = src.ORDERID
-when matched then
-  update set raw_order.shippeddate = src.shippeddate2;
-
--- update raw_order
--- set shippeddate = '1900-01-01'
--- where shippeddate is null;
+MERGE INTO raw_order
+USING (
+    SELECT ORDERID, DATEADD(YEAR, 1900, shippeddate) AS shippeddate2
+    FROM raw_order
+    WHERE shippeddate < '1900-01-01'
+) AS src
+ON raw_order.ORDERID = src.ORDERID
+WHEN MATCHED THEN
+    UPDATE SET raw_order.shippeddate = src.shippeddate2;
