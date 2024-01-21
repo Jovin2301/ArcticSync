6e444eab4705d5010d29119b5b1b37d01e35ba73
Sam <samuelkoh17@gmail.com>
2024-01-22 00:21:19 +0800

    Updated connection settings to own account, and queried in the raw_category duplicate values of CATEGORYID. 8 of such records were queried. Did it in safe environment (dbt test), no dbt run yet.

--- models/raw/raw_category.sql
+++ models/raw/raw_category.sql
@@ -1,3 +1,12 @@
 {{ config(materialized='table') }}
 
 SELECT * FROM {{ source('NWT', 'CATEGORY') }}
+
+/* Issue lies in having duplicated values for CATEGORY ID in 
+raw_category. Hence, we query the duplicated values.
+This shows the CATEGORY ID values that have more than one
+occurrence. We should expect 8 of them. */
+SELECT CATEGORYID, COUNT(*)
+FROM raw_category
+GROUP BY CATEGORYID
+HAVING COUNT(*) > 1;
