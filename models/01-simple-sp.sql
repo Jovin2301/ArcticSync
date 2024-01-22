7297540b6d8043a0d1fcf2c31b057c99797e62b9
Sam <samuelkoh17@gmail.com>
2024-01-23 00:24:58 +0800

    for stored procedures, I switched it to javascript, as it is more compatible for snowflake

--- models/01-simple-sp.sql
+++ models/01-simple-sp.sql
@@ -1,8 +1,14 @@
 -- source from: https://www.youtube.com/watch?v=CsH2sB6gwlw&ab_channel=DataEngineeringSimplified
 
-CREATE PROCEDURE SelectAllEmployees
+CREATE OR REPLACE PROCEDURE SelectAllEmployees()
+    RETURNS STRING
+    LANGUAGE JAVASCRIPT
+    EXECUTE AS CALLER
 AS
-SELECT * FROM Employee
-GO;
-
-EXEC SelectAllEmployees
+$$
+    var sql_command = "SELECT * FROM Employee;";
+    var statement1 = snowflake.createStatement({sqlText: sql_command});
+    var resultSet1 = statement1.execute();
+    return resultSet1;
+$$;
+CALL SelectAllEmployees();
