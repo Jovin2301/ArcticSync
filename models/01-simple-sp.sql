f903bafe6a2421461e8cf9ddbd3ee8724611211c
Sam <samuelkoh17@gmail.com>
2024-01-23 00:47:31 +0800

    Documented the code so my teammates can understand my code better.

--- models/01-simple-sp.sql
+++ models/01-simple-sp.sql
@@ -13,19 +13,23 @@ $$
         //avoid SQL injection
         var sql_command = "SELECT * FROM RAW_ORDER WHERE ORDERID = :1";
         
+        // we create a statement with the parameterised query
         var statement1 = snowflake.createStatement({
             sqlText: sql_command,
             binds: [orderId]
         });
         
+        //executing the statement and fetch results
         var resultSet1 = statement1.execute();
         var result = resultSet1.fetchAll({format: 'json'});
 
+        // lastly, we return the results
         return result;
+
     } catch (err) {
-        
+        // we log the error specifically
         snowflake.execute("INSERT INTO error_log (error_message) VALUES (:1)", [err.message]);
-        
+        // rethrow the error, propagating it to the caller
         throw err;
     }
 $$;
