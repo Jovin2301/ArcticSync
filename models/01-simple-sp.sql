ff2fb3b87b57f3d371e0d4640ae1555bee7d01b4
Sam <samuelkoh17@gmail.com>
2024-01-23 00:37:30 +0800

    Did a switch to querying out the order details, given order id. This allows slightly more meaningful queries.

--- models/01-simple-sp.sql
+++ models/01-simple-sp.sql
@@ -1,14 +1,46 @@
 -- source from: https://www.youtube.com/watch?v=CsH2sB6gwlw&ab_channel=DataEngineeringSimplified
 
-CREATE OR REPLACE PROCEDURE SelectAllEmployees()
-    RETURNS STRING
+-- query for order details, given order ID
+CREATE OR REPLACE PROCEDURE GetOrderDetails(orderId STRING)
+    RETURNS VARIANT
     LANGUAGE JAVASCRIPT
     EXECUTE AS CALLER
 AS
 $$
-    var sql_command = "SELECT * FROM Employee;";
-    var statement1 = snowflake.createStatement({sqlText: sql_command});
+    // Use parameterized query to avoid SQL injection
+    var sql_command = "SELECT * FROM Orders WHERE ORDERID = :1;";
+    
+    // we now use a parameterised query, by creating a statement
+    var statement1 = snowflake.createStatement({
+        sqlText: sql_command, 
+        binds: [ORDERID]
+        });
+    
+    //we then execute the statement, using .execute(), and
+    //saving it as a variable
     var resultSet1 = statement1.execute();
-    return resultSet1;
+    
+    // fetch all rows as a variant (JSON-like) object
+    var result = resultSet1.fetchAll({format: 'json'});
+    
+    // return the result
+    return result;
 $$;
-CALL SelectAllEmployees();
+
+
+
+
+-- simple query to query all employees
+-- -------------------------------------------------------
+-- CREATE OR REPLACE PROCEDURE SelectAllEmployees()
+--     RETURNS STRING
+--     LANGUAGE JAVASCRIPT
+--     EXECUTE AS CALLER
+-- AS
+-- $$
+--     var sql_command = "SELECT * FROM Employee;";
+--     var statement1 = snowflake.createStatement({sqlText: sql_command});
+--     var resultSet1 = statement1.execute();
+--     return resultSet1;
+-- $$;
+-- CALL SelectAllEmployees();
