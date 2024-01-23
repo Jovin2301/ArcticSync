54d9d13fb009dd70df03d8c20f06cd4bb0b15896
NP-Faris-Yeo <148425782+NP-Faris-Yeo@users.noreply.github.com>
2024-01-23 16:09:22 +0800

    Update V1.1.3__merge.sql

--- migrations/V1.1.3__merge.sql
+++ migrations/V1.1.3__merge.sql
@@ -1,7 +1,7 @@
-USE ASSIGNMENT2.NWT
+USE ASSIGNMENT2.NWT;
 merge into PRODUCT as p
 using FRESH_PRODUCT as fp
 on p.ProductID = fp.ProductID
 when MATCHED then
   update set p.UnitCost = fp.UnitCost
-  where p.UnitCost is null
+  where p.UnitCost is null;
