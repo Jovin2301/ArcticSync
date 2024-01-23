31a5a6d7544156c79056622e2031b740052379ed
Sam <samuelkoh17@gmail.com>
2024-01-24 00:24:12 +0800

    Tried uncommenting the shippeddate values to update years less than 1990 to 1990, and dbt test. Still does not work

--- models/raw/raw_order.sql
+++ models/raw/raw_order.sql
@@ -1,18 +1,18 @@
 
--- --{{ config (materialized='table')}}
+--{{ config (materialized='table')}}
 
---SELECT * FROM {{source('NWT', 'ORDERS')}}
+SELECT * FROM {{source('NWT', 'ORDERS')}}
 
--- /*UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
--- WHERE orderdate < '1900-01-01';
+UPDATE raw_order SET orderdate = DATEADD(YEAR, 1900, orderdate) -- add 1900 to dates with years 0096, 0097 and 0098
+WHERE orderdate < '1900-01-01';
 
--- UPDATE raw_order SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
--- WHERE requireddate < '1900-01-01';
+UPDATE raw_order SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
+WHERE requireddate < '1900-01-01';
 
--- UPDATE raw_order SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
--- WHERE shippeddate < '1900-01-01';
+UPDATE raw_order SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
+WHERE shippeddate < '1900-01-01';
 
--- select * from raw_order*/
+select * from raw_order
 {{ config (materialized='table')}}
 
 {{ config(materialized='incremental', unique_key='ORDERID') }}
