4493db10d80c8490182cdfa8d8045e4b0f430444
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-01-24 11:09:16 +0800

    Update V1.1.3__merge.sql

--- migrations/V1.1.3__merge.sql
+++ migrations/V1.1.3__merge.sql
@@ -1,10 +1,18 @@
-USE ASSIGNMENT2.NWT;
+--USE ASSIGNMENT2.NWT;
 
-UPDATE raw_order 
+UPDATE ASSIGNMENT2.NWT.raw_order 
 SET ORDERDATE = DATEADD(YEAR, 1900, ORDERDATE) -- add 1900 to dates with years 0096, 0097 and 0098
 WHERE ORDERDATE < '1900-01-01';
 
-UPDATE raw_order
+UPDATE ASSIGNMENT2.NWT.raw_order 
+SET requireddate = DATEADD(YEAR, 1900, requireddate) -- add 1900 to dates with years 0096, 0097 and 0098
+WHERE requireddate < '1900-01-01';
+
+UPDATE ASSIGNMENT2.NWT.raw_order 
+SET shippeddate = DATEADD(YEAR, 1900, shippeddate) -- add 1900 to dates with years 0096, 0097 and 0098
+WHERE shippeddate < '1900-01-01';
+
+UPDATE ASSIGNMENT2.NWT.raw_order
 SET SHIPPEDDATE = '1970-01-01 00:00:00'
 WHERE SHIPPEDDATE IS NULL;
 
