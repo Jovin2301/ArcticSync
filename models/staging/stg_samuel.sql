a5ddd6d99e5aabd961588b76c4f067b5a51fd9ed
Sam <samuelkoh17@gmail.com>
2024-01-25 13:11:21 +0800

    temporarily skipping staging file

--- models/staging/stg_samuel.sql
+++ models/staging/stg_samuel.sql
@@ -1,67 +1,67 @@
-{{ config(
-   materialized='view',
-) }}
+-- {{ config(
+--    materialized='view',
+-- ) }}
 
-select
---from raw_employeeTerridtory
-e.EmployeeId,
-t.TerritoryId,
+-- select
+-- --from raw_employeeTerridtory
+-- e.EmployeeId,
+-- t.TerritoryId,
 
---from raw_territory
-TerritoryId,
-r.RegionId,
-TerritoryDescription,
+-- --from raw_territory
+-- TerritoryId,
+-- r.RegionId,
+-- TerritoryDescription,
 
---from raw_region
-RegionId,
-RegionDescription,
+-- --from raw_region
+-- RegionId,
+-- RegionDescription,
 
---from raw_employee
-EmployeeId,
-LastName,
-FirstName,
-Title,
-TitleOfCourtesy,
-BirthDate,
-HireDate,
-Address, 
-City,
-Region,
-PostalCode,
-Country,
-HomePhone,
-Extension,
-Photo,
-Notes, ReportsTo, PhotoPath,
+-- --from raw_employee
+-- EmployeeId,
+-- LastName,
+-- FirstName,
+-- Title,
+-- TitleOfCourtesy,
+-- BirthDate,
+-- HireDate,
+-- Address, 
+-- City,
+-- Region,
+-- PostalCode,
+-- Country,
+-- HomePhone,
+-- Extension,
+-- Photo,
+-- Notes, ReportsTo, PhotoPath,
 
--- from raw_order
-CustomerId,
-OrderDate,
-RequiredDate,
-ShippedDate,
-ShipVia,
-Freight,
-ShipName,
-ShipAddress,
-ShipCity,
-ShipRegion,
-ShipPostalCode,
-ShipCountry,
+-- -- from raw_order
+-- CustomerId,
+-- OrderDate,
+-- RequiredDate,
+-- ShippedDate,
+-- ShipVia,
+-- Freight,
+-- ShipName,
+-- ShipAddress,
+-- ShipCity,
+-- ShipRegion,
+-- ShipPostalCode,
+-- ShipCountry,
 
--- from raw_order_details
-ProductId,
-UnitPrice,
-Quantity,
-Discount
+-- -- from raw_order_details
+-- ProductId,
+-- UnitPrice,
+-- Quantity,
+-- Discount
 
-from {{ ref('raw_employee') }} as e
-left join {{ ref('raw_employee_territory') }} as et
-on e.EmployeeId = et.EmployeeId
-left join {{ ref('raw_territory') }} as t
-on t.TerritoryId = et.TerritoryId
-left join {{ ref('raw_region') }} as r
-on r.RegionId = t.RegionId
-left join {{ ref('raw_order')}} as o
-on o.EmployeeId = e.EmployeeId
-left join {{ ref('raw_order_details')}} as od
-on od.OrderId = o.OrderId
+-- FROM {{ ref('raw_employee') }} e
+-- LEFT JOIN {{ ref('raw_employee_territory') }} et 
+-- ON e.EmployeeId = et.EmployeeId
+-- LEFT JOIN {{ ref('raw_territory') }} t 
+-- ON t.TERRITORYID = et.TERRITORYID
+-- LEFT JOIN {{ ref('raw_region') }} r 
+-- ON t.RegionId = r.RegionId
+-- LEFT JOIN {{ ref('raw_order') }} o 
+-- ON e.EmployeeId = o.EmployeeId
+-- LEFT JOIN {{ ref('raw_order_details') }} od 
+-- ON o.OrderId = od.OrderId
