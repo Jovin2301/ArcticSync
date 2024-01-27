ce43f0eec74144fc9d1a4d45d89c2c7ba42c61d0
NP-Faris-Yeo <148425782+NP-Faris-Yeo@users.noreply.github.com>
2024-01-27 14:09:45 +0800

    Update report_top_salesperson_for_client_MARKELL.sql

--- models/reporting/report_top_salesperson_for_client_MARKELL.sql
+++ models/reporting/report_top_salesperson_for_client_MARKELL.sql
@@ -1,4 +1,4 @@
-/*SELECT
+SELECT
 COMPANYNAME,
 FIRSTNAME,
 EMPLOYEEID,
@@ -8,4 +8,3 @@ GROUP BY EMPLOYEEID, FIRSTNAME, COMPANYNAME
 QUALIFY RANK() OVER (PARTITION BY COMPANYNAME ORDER BY COUNT(ORDERID) DESC) = 1  -- firstly, seperates out by company, orders the count of orders for 
 -- employees and ranks them from highest to lowest, choose the highest 
 -- QUALIFY clause is used to filter based on the ranking. For each company, only choose the employee with the highest order count
-*/
