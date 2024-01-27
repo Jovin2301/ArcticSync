2d86055a458b92280644665b33f358dbaa35d419
NP-Faris-Yeo <148425782+NP-Faris-Yeo@users.noreply.github.com>
2024-01-27 12:18:55 +0800

    Update report_sales_by_region.sql

--- models/reporting/report_sales_by_region.sql
+++ models/reporting/report_sales_by_region.sql
@@ -1,6 +1,6 @@
-{{ config(  materialized='table') }}
+/*{{ config(  materialized='table') }}
 
 -- This is a SQL file, not a dbt model file
 SELECT RegionDescription, AVG(UnitPrice) as AvgUnitPrice
 FROM {{ ref('samuel_staging') }}
-GROUP BY RegionDescription
+GROUP BY RegionDescription*/
